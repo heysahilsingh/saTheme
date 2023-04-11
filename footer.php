4b22ed7cea9af2335fe4f6e3e3d3965883553d2c
heysahilsingh <119090408+heysahilsingh@users.noreply.github.com>
2023-04-11 11:55:36 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!-- Site Footer Start -->
+<footer class="sa-site-footer sa-full-width">Footer Data</footer>	
+<!-- Site Footer End -->
+<!-- Site Footer Assets Start -->
+<div class="sa-footer-assets"><?php wp_footer(); ?></div>
+<!-- Site Footer Assets End -->
+</body>
+</html>
+
